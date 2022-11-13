7190831c35c142ab46ad44deaf881b9f6512c8e2
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-13 20:15:40 +0900

    feat: main 리팩토링 및 splash 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:kw_dormitory/constants.dart';
+import 'package:kw_dormitory/screen/splash/splash_screen.dart';
 import 'package:kw_dormitory/util/create_material_color.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const KwDormitoryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class KwDormitoryApp extends StatelessWidget {
+  const KwDormitoryApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,54 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: createMaterialColor(kThemeColor),
           fontFamily: "NanumSquare"),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const SplashScreen(),
     );
   }
 }
