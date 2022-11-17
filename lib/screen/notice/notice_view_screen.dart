8e67aa386ce16106ecb9b7226faeea322dd96a0c
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-17 16:09:21 +0900

    feat: 공지사항 조회 화면 초안 UI 작성

---
+++ lib/screen/notice/notice_view_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoticeViewScreen extends StatefulWidget {
+  const NoticeViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NoticeViewScreen> createState() => _NoticeViewScreenState();
+}
+
+class _NoticeViewScreenState extends State<NoticeViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
