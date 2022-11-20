43da3b3f44c259390d89f65534214f658fe6a96d
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-20 23:01:10 +0900

    feat: kakao sdk 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:kw_dormitory/constants.dart';
 import 'package:kw_dormitory/screen/splash/splash_screen.dart';
 import 'package:kw_dormitory/util/create_material_color.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: "945b79f2950fa2c577939f5724e2c830");
   runApp(const KwDormitoryApp());
 }
 
