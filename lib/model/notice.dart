fb1445e1690045c7c1dbb761ac29eb9059defba2
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-29 17:07:22 +0900

    feat: define notice model

---
+++ lib/model/notice.dart
@@ -0,0 +1,31 @@
+class NoticeResponse {
+  final List<Notice> notices;
+
+  NoticeResponse({required this.notices});
+
+  factory NoticeResponse.fromJson(List<dynamic> json) {
+    List<Notice> notices = json.map((e) => Notice.fromJson(e)).toList();
+    return NoticeResponse(notices: notices);
+  }
+}
+
+class Notice {
+  final int noticeId;
+  final String title;
+  final String writer;
+  final String createdAt;
+
+  Notice(
+      {required this.noticeId,
+      required this.title,
+      required this.writer,
+      required this.createdAt});
+
+  factory Notice.fromJson(Map<String, dynamic> json) {
+    return Notice(
+        noticeId: json["noticeId"],
+        title: json["title"],
+        writer: json["writer"],
+        createdAt: json["createdAt"]);
+  }
+}
