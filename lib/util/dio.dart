f478f4651cf4dee9e4acaa067a309caaeffcbe0d
yjyoon-dev <yjyoon.dev@gmail.com>
2022-12-04 23:32:23 +0900

    feat: api 배포 주소로 변경

--- lib/util/dio.dart
+++ lib/util/dio.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 Dio getDio(String? token) {
   var dio = Dio();
 
-  dio.options.baseUrl = "http://10.0.2.2:8888/api/v1";
+  dio.options.baseUrl = "http://52.78.172.103:8888/api/v1";
 
   if (token != null) {
     dio.options.headers["Authorization"] = "Bearer $token";
